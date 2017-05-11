fc7841bd86a5d11712ed48c3ce258629ff2b00b8
davidd1990 <jibaneze@unbosque.edu.co>
2017-05-11 08:57:15 -0500

    Actualizacion

--- GeneradorPrimos.py
+++ GeneradorPrimos.py
@@ -18,6 +18,7 @@ def encontrarprimos(n):
     p = [2] + [x for x in range(3, n + 1, 2) if
                not [y for y in range(3, int(x ** 0.5) + 1, 2) if (float(x) / y).is_integer()]]
     print(p)
+    print("suma de la lista : ", sum(p))
 
 
 def encontrar_primos_threads(n, z):
@@ -34,9 +35,9 @@ def dividir(x):
     for y in range(3, int(x ** 0.5) + 1, 2):
         if (float(x) / y).is_integer():
             return True
-        #Este es el metodo principal donde corren los Threads y el metodo sin Threads
 
 
+#Este es el metodo principal donde corren los Threads y el metodo sin Threads
 def main():
     n = 100
     global p
@@ -49,10 +50,10 @@ def main():
     # el metedo encontrar_primos_threads tiene dos argumentos el primero es la cantidad final y segundo es de donde
     #empezar. Por ejemplo si quiero 100 como cantidad final puedo empezar desde 10 o desde 50 depende de lo que ustedes
     #deseen.Por ultimo, el valor minimo es 8 no 0
-    t1 = threading.Thread(target=encontrar_primos_threads, args=(500000, 8,))
-    t2 = threading.Thread(target=encontrar_primos_threads, args=(1000000, 500000,))
-    t3 = threading.Thread(target=encontrar_primos_threads, args=(1500000, 1000000,))
-    t4 = threading.Thread(target=encontrar_primos_threads, args=(2000000, 1500000,))
+    t1 = threading.Thread(target=encontrar_primos_threads, args=(50000, 8,))
+    t2 = threading.Thread(target=encontrar_primos_threads, args=(100000, 50000,))
+    t3 = threading.Thread(target=encontrar_primos_threads, args=(150000, 100000,))
+    t4 = threading.Thread(target=encontrar_primos_threads, args=(200000, 150000,))
 
 
     #Cada vez que creen un thread tiene que inicializarlo de la forma que se muestra abajo
@@ -71,12 +72,16 @@ def main():
 
     #Como son varios Threads la lista resultante queda en desorden por lo que hay q ordenarla
     p.sort()
+
     print(p)
-    print("trabajo terminao con threads : ", time.time()-t)
+
+    print("suma de la lista : ", sum(p))
+    print("trabajo terminado con threads : ", time.time()-t)
 
     #Aqui empieza el metodo normal sin ningun Thread
     t = time.time()
-    encontrarprimos(2000000)
-    print("trabajo terminao sin threads en : ", time.time()-t)
+    encontrarprimos(200000)
+
+    print("trabajo terminado sin threads en : ", time.time()-t)
 
 main()
